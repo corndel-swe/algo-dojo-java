43b169b9d4bb570cb0eac844dfbd3f69367a9c7a
Charlie-robin <charlie.robin.richardson@gmail.com>
2025-07-17 16:15:51 +0100

    ArrayFilter added

---
+++ src/main/java/com/corndel/two_pointers/array_filter/ArrayFilter.java
@@ -0,0 +1,43 @@
+package com.corndel.two_pointers.array_filter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayFilter {
+
+    public static int[] getDistinctNumbersNestedLoop(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return new int[0];
+        }
+
+        List<Integer> resultList = new ArrayList<>();
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                resultList.add(arr[i]);
+            }
+        }
+        resultList.add(arr[arr.length - 1]);
+
+        return resultList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static int[] getDistinctNumbersTwoPointers(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return new int[0];
+        }
+        int[] mutableArr = Arrays.copyOf(arr, arr.length);
+
+        int write = 1;
+        for (int read = 1; read < mutableArr.length; read++) {
+
+            if (mutableArr[read] != mutableArr[read - 1]) {
+                mutableArr[write] = mutableArr[read];
+                write++;
+            }
+        }
+
+        return Arrays.copyOf(mutableArr, write);
+    }
+}
---
+++ src/main/java/com/corndel/two_pointers/array_filter/instructions.md
@@ -0,0 +1,29 @@
+# ArrayFilter
+
+## The Problem
+
+You're given a **sorted** array of integers. Your task is to complete the two methods on the `MaxWaterContainer` class so that they return a new array
+where **each element appears only once**, and all duplicates are removed.
+
+The result should preserve the sorted order.
+
+## Naive Solution: `ArrayFilter.getDistinctNumbersNestedLoop`
+
+Iterate through the array and compare each element to the next one. If it’s
+different from the next, add it to a result array.
+
+- Time complexity: O(n)
+- Space complexity: O(n)
+
+## Two-Pointer Solution: `ArrayFilter.getDistinctNumbersTwoPointers`
+
+1. Use a `write` pointer to track where the next unique number should go.
+2. Start `read` from index 1 and scan through the array.
+3. If `arr[read] !== arr[read - 1]`, write it to `arr[write]`, then increment
+   `write`.
+4. At the end, return `arr.slice(0, write)` to get the unique portion.
+
+- Time complexity: O(n)
+- Space complexity: O(1) extra space (excluding the returned slice)
+- This modifies the array in-place and avoids allocating a second array during
+  scanning.
